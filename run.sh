0e4e758c5d0e9d61eb29045249a190c683e09d31
mimictalk <mimictalk@163.com>
2024-09-26 15:31:42 +0800

    update demo page

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 git config user.name "mimictalk"
 git config user.email "mimictalk@163.com"
-# ssh-keygen -t rsa -f ~/.ssh/fs3dportrait_id_rsa
+# ssh-keygen -t rsa -f ~/.ssh/mimictalk_id_rsa
 eval $(ssh-agent)
 ssh-add ~/.ssh/mimictalk_id_rsa
 git remote add origin git@github.com:mimictalk/mimictalk.github.io.git
